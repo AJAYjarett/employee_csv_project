894638758bbe867ee79136e0ec1abbcab1b875d5
Hanif Ali <ali.hanif@outlook.com>
2021-12-20 17:32:01 +0000

    added addEmployeesToList method to EmployeesDAO

--- src/main/java/employee_csv_project/model/EmployeesDAO.java
+++ src/main/java/employee_csv_project/model/EmployeesDAO.java
@@ -1,6 +1,18 @@
 package employee_csv_project.model;
 
+import java.util.ArrayList;
+
 public class EmployeesDAO {
     //stores employees
+
+    ArrayList<EmployeeDTO> allEmployees = new ArrayList<>();
+
+    //places all employees into and ArrayList
+    public void addEmployeesToList(String[] listOfEmployeesAsString){
+        for (int i = 0; i < listOfEmployeesAsString.length; i++) {
+            EmployeeDTO employee = new EmployeeDTO(listOfEmployeesAsString);
+            allEmployees.add(employee);
+        }
+    }
 
 }
