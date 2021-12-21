ea27de6e62928b2496c96d7f322233b76ff78b8b
Harry Jones <harryrjones@yahoo.com>
2021-12-21 16:08:52 +0000

    Instantiated the logger

--- src/main/java/employee_csv_project/app/RuntimeController.java
+++ src/main/java/employee_csv_project/app/RuntimeController.java
@@ -1,8 +1,15 @@
 package employee_csv_project.app;
 
+import employee_csv_project.logger.LogWriter;
+
+import java.util.logging.Level;
+
 public class RuntimeController {
 
     public static void runProgram(){
+        LogWriter.createLogger();
+        LogWriter.writeLog(Level.INFO, "Logging Started");
+
         RuntimeTasks.createEmployeesDAO();
     }
 }
--- src/main/java/employee_csv_project/app/RuntimeTasks.java
+++ src/main/java/employee_csv_project/app/RuntimeTasks.java
@@ -1,19 +1,23 @@
 package employee_csv_project.app;
 
 import employee_csv_project.controller.EmployeeCsvParser;
+import employee_csv_project.logger.LogWriter;
 import employee_csv_project.model.EmployeesDAO;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
 
 public class RuntimeTasks {
 
     public static EmployeesDAO createEmployeesDAO(){
         EmployeesDAO employeesDAO = new EmployeesDAO();
+
+        LogWriter.writeLog(Level.INFO, "Getting employee data from CSV file");
         List<String[]> employeesToStore = EmployeeCsvParser.createEmployeeData();
         for (int i = 0; i < employeesToStore.size(); i++) {
             employeesDAO.addEmployeeToList(employeesToStore.get(i));
-            System.out.println(Arrays.toString(employeesToStore.get(i)));
+//            System.out.println(Arrays.toString(employeesToStore.get(i)));
         }
         return employeesDAO;
     }
