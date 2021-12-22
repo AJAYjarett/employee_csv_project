8e4cae39956a24f96c3e1b191fa84f94157d8224
Harry Jones <harryrjones@yahoo.com>
2021-12-22 09:57:05 +0000

    Updated DataCleaner to return a LocalDate rather than String.

--- src/test/java/employee_csv_project/controller/DataCleanerTest.java
+++ src/test/java/employee_csv_project/controller/DataCleanerTest.java
@@ -5,22 +5,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class DataCleanerTest {
     String badDate = "9/4/2008";
     String goodDate = "10/20/1994";
+    LocalDate badLocalDate = LocalDate.of(2008,9,4);
+    LocalDate goodLocalDate = LocalDate.of(1994,10,20);
 
     @Test
     @DisplayName("Test that incorrectly formatted date is formatted correctly")
     void testThatIncorrectlyFormattedDateIsFormattedCorrectly() {
-        Assertions.assertEquals("09/04/2008", DataCleaner.formatDate(badDate));
+        Assertions.assertEquals(badLocalDate, DataCleaner.formatDate(badDate));
     }
 
     @Test
     @DisplayName("Test that correctly formatted date is unchanged")
     void testThatCorrectlyFormattedDateIsUnchanged() {
-        Assertions.assertEquals(goodDate, DataCleaner.formatDate(goodDate));
+        Assertions.assertEquals(goodLocalDate, DataCleaner.formatDate(goodDate));
     }
 }
