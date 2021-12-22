5f14f67bef321cf99ea97e91d8de6d015b557cfb
PawelTSW90 <pawel.dyjak@poczta.onet.pl>
2021-12-22 11:58:41 +0100

    CheckForDuplicates test added

--- src/test/java/employee_csv_project/controller/CheckForDuplicatesTest.java
+++ src/test/java/employee_csv_project/controller/CheckForDuplicatesTest.java
@@ -1,5 +1,8 @@
 package employee_csv_project.controller;
 
+import employee_csv_project.controller.csv_controller.duplication_check.CheckForDuplicates;
+import employee_csv_project.model.EmployeeDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +11,13 @@ public class CheckForDuplicatesTest {
     @Test
     @DisplayName("Check For Duplicates Test")
     void checkForDuplicatesTest() {
+        String[] employeeData1 = new String[]{"19846","Mrs.","Serafina","I","Bumgarner","F","serafina.bumgarner@exxonmobil.com","9/21/1982","2/1/2008","69294"};
+        String[] employeeData2 = new String[]{"19846","Mrs.","Juliette","M","Rojo","F","juliette.rojo@yahoo.co.uk","5/8/1967","6/4/2011","193912"};
+
+        EmployeeDTO employee = new EmployeeDTO(employeeData1);
+        EmployeeDTO employee2 = new EmployeeDTO(employeeData2);
+        CheckForDuplicates.checkForIdDuplicates(employee);
+        Assertions.assertEquals(false, CheckForDuplicates.checkForIdDuplicates(employee2));
 
 
     }
